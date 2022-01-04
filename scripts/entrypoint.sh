fe975efb070a4ff332aa961f5df64363cba423fc
Erik Pearson <eapearson@lbl.gov>
2022-01-04 06:54:14 -0800

    remove test entrypoint, fix develop

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -30,20 +30,17 @@ else
     workers=17
     log_level=info
   elif [ "${1}" = "develop" ] ; then
-    # Prepare a test database, so that we have a working database
-    # for local development
-    python "${script_dir}/../lib/cli/prepare-arango.py"
-    workers=1
-    log_level=debugr
- 
-  elif [ "${1}" = "test" ] ; then
     # Python path must include the test directory in order for the test validators
     # to be loadable.
+    # Test directory is included in the python path so that we can use
+    # test validators defined therein. 
+    # TODO: we may want to establish a dev directory in which to place
+    # development validators, data for loading, etc.
     export PYTHONPATH="$script_dir/../test:$PYTHONPATH"
-    echo "[ENTRYPOINT] test mode with Python path: ${PYTHONPATH}"
+    echo "[ENTRYPOINT] develop mode with Python path: ${PYTHONPATH}"
     python "${script_dir}/../lib/cli/prepare-arango.py"
     workers=1
-    log_level=info
+    log_level=debug
   else
     echo "Unknown entrypoint option: ${1}"
     exit 1
