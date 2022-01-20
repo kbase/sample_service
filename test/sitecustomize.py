255aa005f4bac8b72595941d36c7a878d855fcbb
Erik Pearson <eapearson@lbl.gov>
2022-01-20 10:17:37 -0800

    improve logging from sitecustomize

--- test/sitecustomize.py
+++ test/sitecustomize.py
@@ -2,6 +2,12 @@ import coverage
 import os
 
 if "COVERAGE_PROCESS_START" in os.environ:
-    print("COVERAGE_PROCESS_START: ", os.environ["COVERAGE_PROCESS_START"])
+    print(
+        "[sitecustomize] coverage will run; COVERAGE_PROCESS_START: ",
+        os.environ["COVERAGE_PROCESS_START"],
+        flush=True,
+    )
 
     coverage.process_startup()
+else:
+    print("[sitecustomize] coverage not running", flush=True)
